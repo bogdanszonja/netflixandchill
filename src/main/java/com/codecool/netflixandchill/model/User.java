d8175419e0ce29f8f76932d3c510194f0420f858
Grob Oliver <groboliver1117@gmail.com>
2018-10-03 14:20:30 +0200

    User entity created

--- src/main/java/com/codecool/netflixandchill/model/User.java
+++ src/main/java/com/codecool/netflixandchill/model/User.java
@@ -1,21 +1,40 @@
 package com.codecool.netflixandchill.model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    private String userName;
+    private String nickname;
 
+    @Column(unique = true)
     private String emailAddress;
 
     private String password;
 
-    private List<Episode> watchedEpisodes;
+    @ManyToMany
+    private List<Episode> watchedEpisodes = new ArrayList<>();
 
+    @Temporal(TemporalType.DATE)
     private Date registrationDate;
+
+    public User() {
+    }
+
+    public User(String nickname, String emailAddress, String password, Date registrationDate) {
+        this.nickname = nickname;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.registrationDate = registrationDate;
+    }
 
     public long getId() {
         return id;
@@ -25,12 +44,12 @@ public class User {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getNickname() {
+        return nickname;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setNickname(String userName) {
+        this.nickname = userName;
     }
 
     public String getEmailAddress() {
@@ -55,6 +74,10 @@ public class User {
 
     public void setWatchedEpisodes(List<Episode> watchedEpisodes) {
         this.watchedEpisodes = watchedEpisodes;
+    }
+
+    public void addEpisode(Episode episode) {
+        watchedEpisodes.add(episode);
     }
 
     public Date getRegistrationDate() {
