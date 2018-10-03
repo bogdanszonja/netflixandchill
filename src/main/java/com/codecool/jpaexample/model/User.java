3c3b66c382532fc1f055b8cb330fd6709c626294
anitaSzabo <azorka3@gmail.com>
2018-10-03 10:07:20 +0200

    empty classes and enums created

--- src/main/java/com/codecool/jpaexample/model/User.java
+++ src/main/java/com/codecool/jpaexample/model/User.java
@@ -1,4 +1,14 @@
 package com.codecool.jpaexample.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class User {
+
+    private long id;
+    private String userName;
+    private String emailAddress;
+    private String password;
+    private List<Episode> watchedEpisodes;
+    private Date registrationDate;
 }
