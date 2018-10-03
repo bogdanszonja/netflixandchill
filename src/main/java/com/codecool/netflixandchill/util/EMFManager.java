b56cbf7540d4d58c87ff02a47f169cd8e5989558
Grob Oliver <groboliver1117@gmail.com>
2018-10-03 16:11:31 +0200

    EntityManagerFactory made to singleton

---
+++ src/main/java/com/codecool/netflixandchill/util/EMFManager.java
@@ -0,0 +1,29 @@
+package com.codecool.netflixandchill.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EMFManager {
+
+    private static EntityManagerFactory instance = null;
+    
+    public static EntityManagerFactory getInstance() {
+        if (instance == null) {
+            Map<String, String> env = System.getenv();
+            Map<String, Object> configOverrides = new HashMap<>();
+            for (String envName : env.keySet()) {
+                if (envName.contains("DB_USER_NAME")) {
+                    configOverrides.put("javax.persistence.jdbc.user", env.get(envName));
+                }
+                if (envName.contains("DB_PASSWORD")) {
+                    configOverrides.put("javax.persistence.jdbc.password", env.get(envName));
+                }
+            }
+
+            instance = Persistence.createEntityManagerFactory("netflixandchill", configOverrides);
+        }
+        return instance;
+    }
+}
