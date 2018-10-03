115db371d87a2dcda7e23d4f3894e71e4065355c
anitaSzabo <azorka3@gmail.com>
2018-10-03 10:21:10 +0200

    getters and setters created

--- src/main/java/com/codecool/netflixandchill/model/Series.java
+++ src/main/java/com/codecool/netflixandchill/model/Series.java
@@ -12,4 +12,60 @@ public class Series {
     private Date airDate;
     private List<Season> seasons;
     private List<Genre> genres;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getAirDate() {
+        return airDate;
+    }
+
+    public void setAirDate(Date airDate) {
+        this.airDate = airDate;
+    }
+
+    public List<Season> getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(List<Season> seasons) {
+        this.seasons = seasons;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
 }
